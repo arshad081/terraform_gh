c08d25bca02c7fb37881a11263411d16a4746bd3
Arshad <88338790+arshad081@users.noreply.github.com>
2023-07-19 20:54:16 +0530

    Update installjenkins.sh
    working file in amazon linux

--- installjenkins.sh
+++ installjenkins.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 sudo yum update -y
-sudo yum install java-1.8.0-openjdk -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo dnf install java-11-amazon-corretto -y
 sudo yum install jenkins -y
-sudo service jenkins start
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
               
